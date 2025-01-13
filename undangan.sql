f5b4c83fb8c305bcee94336d025c2685e78e5546
Alif ori <flodev261123@gmail.com>
2025-01-14 01:21:45 +0700

    Update Php with sql

---
+++ undangan.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Jan 2025 pada 19.21
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `undangan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `recipients`
+--
+
+CREATE TABLE `recipients` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `recipients`
+--
+
+INSERT INTO `recipients` (`id`, `name`, `created_at`) VALUES
+(1, 'Alif Haikal', '2025-01-13 14:38:59'),
+(2, 'dodi', '2025-01-13 14:40:46'),
+(3, 'tian', '2025-01-13 15:20:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `recipients`
+--
+ALTER TABLE `recipients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `recipients`
+--
+ALTER TABLE `recipients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
